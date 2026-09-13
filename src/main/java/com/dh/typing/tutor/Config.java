b2545dc6af33aa8c131ca81425c2dd2c176aa9f1
danielh281 <daniel.haddadeen1@gmail.com>
2026-09-13 19:31:29 -0400

    Added Config Class

---
+++ src/main/java/com/dh/typing/tutor/Config.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dh.typing.tutor;
+
+/**
+ * 
+ * @author Daniel Haddadeen
+ */
+public class Config {
+    public static String[] typingSentences = new String[] {
+        "Try typing this test. Do it as quickly and accurately as you can.",
+        "Next type another line of input data.",
+        "The quick brown fox jumps over the lazy dog",
+        "Five big quacking zephyrs jolt my wax bed",
+        "Sympathizing would fix Quaker objectives",
+        "A large fawn jumped quickly over white zinc boxes."
+    };
+}
